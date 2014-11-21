5a556d109cdb2cbd87d69ecae6a44c55e3f6b138
Erik Fried <erik.fried@aftonbladet.se>
2014-11-21 10:33:37 +0100

    Corrected migrations + added unique index

--- migrations/20141120135229-tables.js
+++ migrations/20141120135229-tables.js
@@ -4,34 +4,37 @@ var type = dbm.dataType;
 
 exports.up = function (db, callback) {
     async.series([
-    db.createTable('radar', {
-        id: { type: 'int', primaryKey: true, autoIncrement: true },
-        name: { type: 'string', length: 255, notNull: true },
-        createdAt: { type: 'timestamp', notNull: true },
-        updatedAt: { type: 'timestamp', notNull: true }
-    }, callback),
-    db.createTable('target', {
-        id: { type: 'int', primaryKey: true, autoIncrement: true },
-        name: { type: 'string', length: 255, notNull: true },
-        category: { type: 'string', length: 255, notNull: true },
-        description: { type: 'text'},
-        createdAt: { type: 'timestamp', notNull: true },
-        updatedAt: { type: 'timestamp', notNull: true }
-    }, callback),
-    db.createTable('blip', {
-        target_id: { type: 'int', notNull: true},
-        radar_id: { type: 'int', notNull: true},
-        distance: { type: 'int', notNull: true},
-        comment: { type: 'text'},
-        createdAt: { type: 'timestamp', notNull: true },
-        updatedAt: { type: 'timestamp', notNull: true }
-    })
+        db.createTable.bind(db, 'radar', {
+            id: { type: 'int', primaryKey: true, autoIncrement: true },
+            name: { type: 'string', length: 255, notNull: true },
+            created: { type: 'timestamp', notNull: true },
+            updated: { type: 'timestamp', notNull: true }
+        }),
+        db.createTable.bind(db, 'target', {
+            id: { type: 'int', primaryKey: true, autoIncrement: true },
+            name: { type: 'string', length: 255, notNull: true },
+            category: { type: 'string', length: 255, notNull: true },
+            description: { type: 'text'},
+            created: { type: 'timestamp', notNull: true },
+            updated: { type: 'timestamp', notNull: true }
+        }),
+        db.createTable.bind(db, 'blip', {
+            target_id: { type: 'int', notNull: true},
+            radar_id: { type: 'int', notNull: true},
+            distance: { type: 'int', notNull: true},
+            comment: { type: 'text'},
+            created: { type: 'timestamp', notNull: true },
+            updated: { type: 'timestamp', notNull: true }
+        }),
+        db.addIndex.bind(db, 'blip', 'blip_key', ['target_id', 'radar_id'], true)
     ], callback);
 
 };
 
 exports.down = function (db, callback) {
-    db.dropTable('radar', callback);
-    db.dropTable('target', callback);
-    db.dropTable('blip', callback);
+    async.series([
+        db.dropTable.bind(db, 'radar'),
+        db.dropTable.bind(db, 'target'),
+        db.dropTable.bind(db, 'blip')
+    ], callback);
 };
